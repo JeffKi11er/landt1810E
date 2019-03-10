11bb7c3456fb288193711990403b3676782936a3
T3H <dathalongbay@gmail.com>
2019-03-10 18:09:15 +0700

    android buoi 6

---
+++ Android/Buoi6/app/src/main/java/com/phongbm/buoi6/utils/Constances.java
@@ -0,0 +1,9 @@
+package com.phongbm.buoi6.utils;
+
+public interface Constances {
+    String ITEM = "item";
+    String TITLE = "title";
+    String DESC = "description";
+    String PUB_DATE = "pubDate";
+    String LINK = "link";
+}
